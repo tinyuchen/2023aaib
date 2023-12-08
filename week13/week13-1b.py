11c7d09c33bea405dc565edd642505a7203b426d
tinyuchen <tychen.grace@gmail.com>
2023-12-08 11:49:46 +0800

    week13

---
+++ week13/week13-1b.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        #<w1>
+        return "".join(word1)=="".join(word2)
+        
+        #<w2>
+        #a="".join(word1)
+        #b="".join(word2)
+
+        #if a==b: 
+        #    return True
+        #else: 
+        #    return False 
